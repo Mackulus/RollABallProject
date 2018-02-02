8b9e2c38500263bac188a0f2f514361189f6ffd2
Bereim <35741860+Bereim@users.noreply.github.com>
2018-02-02 16:37:06 -0600

    Set up for Menu Scene
    + Made Player a Prefab
    + Made Menu scene and added basic functionality to it.

--- RollABall/Assets/Scripts/CameraController.cs
+++ RollABall/Assets/Scripts/CameraController.cs
@@ -4,17 +4,17 @@ using UnityEngine;
 
 public class CameraController : MonoBehaviour {
 
-	public GameObject player;
+	public GameObject focus;
 
 	private Vector3 offset;
 
 	// Use this for initialization
 	void Start () {
-		offset = transform.position - player.transform.position;
+		offset = transform.position - focus.transform.position;
 	}
 	
 	// Update is called once per frame
 	void LateUpdate () {
-		transform.position = player.transform.position + offset;
+		transform.position = focus.transform.position + offset;
 	}
 }
