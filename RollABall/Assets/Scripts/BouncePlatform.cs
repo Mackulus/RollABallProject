669d108da894bf6b276ec11f5cfeda4495a40fd4
Mack Peters <mpeters@harding.edu>
2018-02-03 13:58:23 -0600

    Started working level 3
    Started playing with a bounce platform

---
+++ RollABall/Assets/Scripts/BouncePlatform.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BouncePlatform : MonoBehaviour {
+
+
+	#region Code from answers.unity.com/questions/773960/trying-to-make-a-bounce-platform-that-tosses-the-p.html
+	public float bounciness; // Determines jump height
+
+	void OnCollisionEnter(Collision other)
+	{
+		other.rigidbody.AddForce(Vector3.up * bounciness, ForceMode.Impulse);
+	}
+
+	#endregion
+}
