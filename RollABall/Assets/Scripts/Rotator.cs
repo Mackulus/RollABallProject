e7038f597e72449352a21fd28578b22069c32192
Bereim <35741860+Bereim@users.noreply.github.com>
2018-02-04 14:25:43 -0600

    Random Additions
    + Added a theoretically working QuitGame script.
    + Changed Rotator to rotate randomly.
    + Added the Menu Scene to the Build Path.
    + Made minor changes to the Menu Scene.

--- RollABall/Assets/Scripts/Rotator.cs
+++ RollABall/Assets/Scripts/Rotator.cs
@@ -1,12 +1,14 @@
-﻿using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 
 public class Rotator : MonoBehaviour {
 
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
 	// Update is called once per frame
-	void Update () 
-	{
-		transform.Rotate (new Vector3 (15, 30, 45) * Time.deltaTime);
+	void Update () {
+        transform.Rotate(new Vector3(15,30,45) * Time.deltaTime * Random.value * 2);
 	}
 }
