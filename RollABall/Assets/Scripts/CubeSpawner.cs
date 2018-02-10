e1b0eb6deda722ba08ff286a59e3cfba15bd3c94
Mack Peters <mpeters@harding.edu>
2018-02-09 23:39:43 -0600

    Added a spawner that shoots cubes
    -Main obstacle for level 4
    -Will finish up rest of level 4 and clean up rest of levels next

---
+++ RollABall/Assets/Scripts/CubeSpawner.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CubeSpawner : MonoBehaviour {
+
+	//Projectile shooting code
+	//https://unity3d.com/learn/tutorials/temas/multiplayer-networking/shooting-single-player
+
+	public GameObject cubePrefab;
+	public Transform cubeSpawn;
+	public float spawnTime = 5f;
+	public int cubeSpeed;
+
+	// Use this for initialization
+	void Start () {
+		InvokeRepeating ("SpawnCube", spawnTime, spawnTime);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	}
+
+	void SpawnCube()
+	{
+		var cube = (GameObject)Instantiate (
+			cubePrefab,
+			cubeSpawn.position,
+			cubeSpawn.rotation);
+
+		cube.GetComponent<Rigidbody>().velocity = cube.transform.forward * cubeSpeed;
+
+		Destroy(cube, spawnTime);
+	}
+}
