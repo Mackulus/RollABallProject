b12753d0aa4e2367c2867cb7463cab1000791c1c
Bereim <35741860+Bereim@users.noreply.github.com>
2018-02-11 15:30:46 -0600

    Cited a Source

--- RollABall/Assets/Scripts/FocusController.cs
+++ RollABall/Assets/Scripts/FocusController.cs
@@ -4,6 +4,7 @@ public class FocusController : MonoBehaviour {
 	public GameObject focus;
 
 	void LateUpdate() {
+		//https://answers.unity.com/questions/132592/lookat-in-opposite-direction.html
 		transform.LookAt(2 * transform.position - focus.transform.position);
 	}
 }
